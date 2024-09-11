0f9e8569cb27f4bcde374f1e790d83d5b02aa7c5
ShivaTejaHitam <61039007+ShivaTejaHitam@users.noreply.github.com>
2024-09-11 20:10:35 +0530

    Update ComputerBuilder.java

--- builder-design-pattern/ComputerBuilder.java
+++ builder-design-pattern/ComputerBuilder.java
@@ -7,6 +7,7 @@
     The pattern organizes object construction into a set of steps (buildWalls, buildDoor, etc.). 
         To create an object, you execute a series of these steps on a builder object. The important part is that you don’t need to call all of the steps. 
         You can call only those steps that are necessary for producing a particular configuration of an object.
+    You can pass different builders to director which is responsible for executing steps in order.
     Builder design pattern can be implemented with or without Director. When Director is not used we can call builder in client code itself
 */
 
