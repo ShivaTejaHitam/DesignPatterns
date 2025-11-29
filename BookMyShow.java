ed41aaeb3623f91dc658d901ff3a564ea59b2133
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2025-11-30 00:28:42 +0530

    Create BookMyShow.java

---
+++ BookMyShow.java
@@ -0,0 +1,30 @@
+class Theatre {
+  private String theatreId;
+  private String cityId;
+  private String theatreName;
+}
+
+class Ticket{
+  private String ticketId;
+  private String showId;
+  private String price;
+}
+
+class Show {
+  private Instant startTime;
+  private Instant endTime;
+  private String theatreId;
+  
+}
+
+class Movie {
+}
+
+class City {
+  private String name;
+}
+
+
+class MovieTheatre {
+
+}
