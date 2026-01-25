db691c1612f8ec66b8106d6d0d4ec8725e8c2a5d
Shiva Teja Bandaru <61039007+ShivaTejaHitam@users.noreply.github.com>
2026-01-25 12:03:16 +0530

    Update ParkingLot.java

--- ParkingLot.java
+++ ParkingLot.java
@@ -30,16 +30,129 @@ public abstract class Vehicle {
   
 }
 
+class Car extends Vehicle {
+    public Car(String licenseNumber) {
+        super(licenseNumber, VehicleType.CAR);
+    }
+}
+
+class Bike extends Vehicle {
+    public Bike(String licenseNumber) {
+        super(licenseNumber, VehicleType.BIKE);
+    }
+}
+
+class Truck extends Vehicle {
+    public Truck(String licenseNumber){
+        super(licenseNumber, VechicleType.TRUCK);
+    }
+}
+
+
 public class ParkingSpot {
+    private ParkingSpotType type;
+    private Vehicle vehicle;
+    private boolean isFree;
 
+    public ParkingSpot(ParkingSpotType type){
+        this.type = type;
+        this.isFree = true;
+    }
+    
+    public ParkingSpotType getType() {
+        return this.type;
+    }
+
+    public boolean canFitVehicle(Vehicle vehicle){
+        return isFree && this.type.equals(vehicle.getType());
+    }
+
+    public void parkVehicle(Vehicle vehicle){
+        this.vehicle = vehicle;
+        this.isFree = false;
+    }
+
+    public void unParkVehicle(Vehicle vehicle){
+        this.vehicle = null;
+        this.isFree = true;
+    }
 }
 
-public abstract class ParkingFloor {
-  private String floorNumber;
+public class ParkingFloor {
+  private String floorId;
   private List<ParkingSpot> parkingSpots;
-
-  // find free spot
-  
+    
+  public Optional<ParkingSpot> getAvailableSpot(Vehicle vehicle) {
+      for(ParkingSpot parkingSpot : parkingSpots){
+          if(parkingSpot.canFitVehicle(vehicle){
+              return parkingSpot;
+          }
+      }
+      return Optional.empty();
+  }
 }
 
-public 
+public ParkingLot {
+    private List<ParkingFloor> parkingFloors;
+    private FeeCalculator feeCalculator;
+
+    public ParkingLot(List<ParkingFloor> parkingFloors,FeeCalculator feeCalculator){
+        this.parkingFloors = parkingFloors;
+        this.feeCalculator = feeCalculator;
+    }
+
+    public ParkngTicket parkVehicle(Vehicle vehicle){
+        for(ParkingFloor parkingFloor : parkingFloors){'
+            ParkingSpot parkingSpot = getAvailableSpot(vehicle);
+            if(parkingSpot.isPresent()){
+                parkingSpot.parkVehicle(vehicle);
+                return ParkingTicket(UUID.random().toString(),parkingSpot);
+            }
+        }
+        throw new ParkingSpotNotFoundException("No parking spot available);
+    }
+
+    public double unParkVehicle(ParkngTicket ParkngTicket){
+        long exitTime = System.currrentTimeMillis();
+        return feeCalculator.CalculateFee(exitTime,parkingTicket.getEntryTime());
+    }
+    
+
+}
+
+class ParkingTicket {
+    private String ticketId;
+    private long entryTime;
+    private ParkingSpot spot;
+
+    public ParkingTicket(String ticketId, ParkingSpot spot) {
+        this.ticketId = ticketId;
+        this.spot = spot;
+        this.entryTime = System.currentTimeMillis();
+    }
+
+    public long getEntryTime() {
+        return entryTime;
+    }
+
+    public ParkingSpot getSpot() {
+        return spot;
+    }
+}
+
+interface FeeCalculator {
+    double calculateFee(long entryTime, long exitTime);
+}
+
+class HourlyFeeCalculator implements FeeCalculator {
+    private static final double RATE_PER_HOUR = 50;
+
+    @Override
+    public double calculateFee(long entryTime, long exitTime) {
+        long duration = exitTime - entryTime;
+        long hours = (long) Math.ceil(duration / (1000.0 * 60 * 60));
+        return hours * RATE_PER_HOUR;
+    }
+}
+
+
